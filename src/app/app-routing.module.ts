29aa7e089585f5e98fc24a81e818882790ac76c1
Hieu Nguyen <hieunv@ait.com>
2022-12-08 13:40:01 +0700

    init commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CustomPreloadingWithDelayStrategy} from "./custom-preloading-with-delay-strategy";
+import {PageNotFoundComponent} from "./page-not-found.component";
+
+const routes: Routes = [
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.module').then(mod => mod.CountryModule),
+    data: { preload: true },
+  },
+  {
+    path: 'person',
+    loadChildren: () => import('./person/person.module').then(mod => mod.PersonModule),
+    data: { preload: true, delay: true, },
+  },
+  {
+    path: 'address',
+    loadChildren: () => import('./address/address.module').then(mod => mod.AddressModule),
+  },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];;
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadingWithDelayStrategy,
+  })],
+  exports: [RouterModule],
+  providers: [CustomPreloadingWithDelayStrategy]
+})
+export class AppRoutingModule { }
